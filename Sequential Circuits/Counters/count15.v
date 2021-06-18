995bd22a8b6b3e2592f31097cb6e29a5f5a614d1
Deepanjali Mishra <71490671+codver@users.noreply.github.com>
2021-06-18 11:26:54 +0530

    Add files via upload

--- Sequential Circuits/Counters/count15.v
+++ Sequential Circuits/Counters/count15.v
@@ -2,9 +2,9 @@
 `timescale 1ns / 1ps
 
 module top_module (
-    input                clk,
-    input                reset,      // Synchronous active-high reset
-    output reg [3:0]     q=4'b0000);
+    input        clk,
+    input        reset,      // Synchronous active-high reset
+    output reg   [3:0] q=4'b0000);
     
     always @(posedge clk) begin
         if(reset)  q <= 4'b0;
