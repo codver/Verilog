a5957a33c82c3217d2032bbcc2e275067361f96d
Deepanjali Mishra <71490671+codver@users.noreply.github.com>
2021-06-18 11:15:48 +0530

    Add files via upload

---
+++ Sequential Circuits/Counters/count15.v
@@ -0,0 +1,14 @@
+//A binary counter from 0-15  11:05 18-06-2021 FRI
+`timescale 1ns / 1ps
+
+module top_module (
+    input        clk,
+    input        reset,      // Synchronous active-high reset
+    output reg   [3:0] q=4'b0000);
+    
+    always @(posedge clk) begin
+        if(reset)  q <= 4'b0;
+        else       q <= q + 1'b1;
+    end
+
+endmodule
